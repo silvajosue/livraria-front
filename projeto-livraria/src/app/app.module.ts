93216594cf6d3160d51a7a4962e7d02264c0d95b
josue.silva <josue.silva>
2021-03-07 22:48:42 -0300

    att

--- projeto-livraria/src/app/app.module.ts
+++ projeto-livraria/src/app/app.module.ts
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
-=======
->>>>>>> 01f81e242ffe81594b8022fe35117c820dfb3e7b
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { MenubarModule } from 'primeng';
-=======
 
-import {MenubarModule} from 'primeng-lts/menubar';
->>>>>>> 01f81e242ffe81594b8022fe35117c820dfb3e7b
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
